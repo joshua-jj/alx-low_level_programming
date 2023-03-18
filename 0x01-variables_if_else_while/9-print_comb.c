5755572a52bbcfed9badb4c596443ca8f1eb36dd
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-18 23:13:11 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,13 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i < 57) 
+		putchar(i);
+		if (i < 57)
 		{
-			putchar(i);
-			putchar(44);
-			putchar(32);
-		}
-		else
-		{
-			putchar(i);
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
