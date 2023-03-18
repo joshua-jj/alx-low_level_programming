f0130c99c282b78f470f6b690d76aeec55669dea
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-18 03:54:30 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n = 97;
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
