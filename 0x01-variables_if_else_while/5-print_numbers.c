636b86ad2a633d62ea3f23ccce61d2d3d90163ed
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-18 22:44:24 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always return 0 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
