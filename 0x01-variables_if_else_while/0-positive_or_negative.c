562d9eb9ab96931adefd46db008ee1a9336e3aaf
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-18 02:59:53 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{
     printf("%d is positive", n);
-}
 else if (n < 0)
-{
     printf("%d is negative", n);
-}
-else{
+else
     printf("%d is zero", n);
-}
 return (0);
 }
