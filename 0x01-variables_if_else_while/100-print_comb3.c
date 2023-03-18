c845c1a03778def238b37db6a738fbc0e3d991d0
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-19 00:41:22 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{	
+			if (i < j)
+			{
+			putchar(i);
+			putchar(j);
+				if (i != 56 || j != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
