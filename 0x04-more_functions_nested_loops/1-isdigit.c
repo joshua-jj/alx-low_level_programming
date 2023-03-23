7a5f724cfd9b9fdbd40119c4ce21ea2ff11cc4cd
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-23 14:37:12 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - check if input is in upper case.
- * @c: input to be checked
- * Return: 1 if input is in uppercase, otherwise  0.
+ * _isdigit - check if input is a digit.
+ * @c: input.
+ * Return: 1 if input is a digit, otherwise  0.
  */
 int _isdigit(int c)
 {
