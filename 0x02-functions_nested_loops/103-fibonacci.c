b777c98dd17de61b8d74da0dc7f0959ef4e5630b
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-23 13:46:43 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - print the first 50 Fibonacci numbers, starting with 1 and 2
+ * main - finds and print the sum of the even valued terms
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void) {
-    // initialize the first two terms of the Fibonacci sequence
-    int prev = 1;
-    int curr = 2;
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
 
-    // initialize the sum of even-valued terms
-    int even_sum = 0;
-
-    // loop until the current term exceeds 4 million
-    while (curr <= 4000000) {
-        // check if the current term is even
-        if (curr % 2 == 0) {
-            even_sum += curr;
-        }
-
-        // calculate the next term of the Fibonacci sequence
-        int next = prev + curr;
-        prev = curr;
-        curr = next;
-    }
-
-    // print the sum of even-valued terms
-    printf("%d\n", even_sum);
-
-    return (0);
+	j = 1;
+	k = 2;
+	sum = 0;
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
 }
