d0bd19fd082c732ba821414967db804c85f206b5
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-23 14:36:03 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if input is in upper case.
+ * @c: input to be checked
+ * Return: 1 if input is in uppercase, otherwise  0.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
