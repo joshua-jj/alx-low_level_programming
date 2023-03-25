9ec6a44e14d49dd1fcf9922c0c8fa728d2ca8460
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-03-25 03:03:07 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line _ draws straight line
+ * @n: number of times _ should be printed
+ */
+void print_line(int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
