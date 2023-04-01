04e8fc9d6f2ed194f34884cb1e1e3a4d639d8a90
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-01 15:36:54 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string that should be checked
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
