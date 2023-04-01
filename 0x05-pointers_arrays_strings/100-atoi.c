7f3e78802ee901cdd9f4f61a5e677cfe1d71f084
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-01 16:57:30 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,35 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src into dest
- * @dest: destination
- * @src: source
- * Return: char with copy of string
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
-
-char *_strcpy(char *dest, char *src)
+int _atoi(char *s)
 {
-	int i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	for (i = 0; *(src + i) != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		dest[i] = *(src + i);
-	}
-	dest[i] = '\0';
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
-	return (dest);
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
