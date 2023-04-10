a5a34862ed37906fb8b0555573a0319b48ff94d9
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-10 02:30:30 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,8 +8,8 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temp;
-	for (int i = 0; i < n / 2; i++)
+	int i, temp;
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = *(a + i);
 		*(a + i) = *(a + n - 1 - i);
