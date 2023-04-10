6d720c9c966474a8caf2211483f4b545ff3ffaf3
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-10 02:20:02 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array to be reversed
+ * @n: array length
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+    for (int i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
