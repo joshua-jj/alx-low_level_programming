a0be0fdff3b9c050f2cd14cd2608a141982a95c0
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-15 17:50:08 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string to be transformed
+ * Return: transformed string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (*(s + i) == sep_words[j])
+			{
+				if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+				{
+					*(s + i + 1) = *(s + i + 1) - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
