f7fbcf7e0a02ad0d9579dc2e6ad3c4a3b753147b
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-15 22:20:39 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,17 +7,17 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + i) >= 97 && *(s + i) <= 122)
-		{
-			*(s + i) = *(s + i) - 32;
-		}
-
-	for (i; *(s + i); i++)
 	{
-		for (j; j < 13; j++)
+		*(s + i) = *(s + i) - 32;
+	}
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 13; j++)
 		{
 			if (*(s + i) == sep_words[j])
 			{
