bfd7bab200ff9d091374cd6d3a38e0e63d5fd2a4
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2023-04-15 17:18:55 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *s)
 
 	for (i = 0; *(s + i); i++)
 	{
-		if(*(s + i) >= 97 && *(s + i) <= 122)
+		if (*(s + i) >= 97 && *(s + i) <= 122)
 		{
 			*(s + i) = *(s + i) - 32;
 		}
